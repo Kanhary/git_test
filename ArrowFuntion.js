26c0ac923f5f4d56bef7e42c8fde04c2b516afbb
Kagnary <128480273+Kanhary@users.noreply.github.com>
2024-08-06 10:02:51 +0700

    first commit

---
+++ ArrowFuntion.js
@@ -0,0 +1,30 @@
+//======basic syntax======
+//const functionName = (parameter) => XPathExpression;
+
+//======no parameter======
+// const noPar = () => 'hello';
+// console.log(noPar());
+
+
+//======Single parameter======
+// const signleParameter = x => x*x;
+// console.log(singleParameter(4));
+
+//======multiple parameter======
+// const multiplePar = (x,y) => x/y;
+// console.log(multiplePar(8,9));
+
+//======Function body======
+// const funcBody = (a,b) =>{
+//     const sub = a-b;
+//     console.log(`Sub result is : ${sub}`);
+//     return sub;
+// }
+// console.log(funcBody(6,7));
+
+//======Returning Object Literals======
+// const createUser = (name, age) => ({name,age});
+// console.log(createUser('Kanha',18));
+
+
+  
